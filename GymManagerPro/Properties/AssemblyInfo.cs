f66096e2e2bf5f3b174deba1a9e8b7ea956f5e68
Steve <cyberstelios@gmail.com>
2014-04-13 18:49:41 +0300

    Changes to assembly info, removed unnecessary files

--- GymManagerPro/Properties/AssemblyInfo.cs
+++ GymManagerPro/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GymManagerPro")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Gym Manager Pro")]
+[assembly: AssemblyDescription("Member Management Software")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyCompany("Steve Alogaris")]
 [assembly: AssemblyProduct("GymManagerPro")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2014")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
